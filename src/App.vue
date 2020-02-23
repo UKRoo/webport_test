e15c110fb683dbd8f8517396173a8aa66a4a5bf3
Dmytro Ivashko <ivashko.dmytro@gmail.com>
2020-02-23 19:53:05 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,98 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <h1><router-link to="/">All Posts</router-link></h1>
+    {{test}}
+    <div>{{ $route.params.id }}</div>
+    <div class="pagination">
+      <a href="#">&laquo;</a>
+      <router-link :to="`${page}`" v-for="page in pages" :key="page" active-class="active">{{ page }}</router-link>
+      <a href="#">&raquo;</a>
     </div>
-    <router-view/>
+    <hr />
+    <div class="post" v-for="(post, index) in showPosts" :key="post.id">
+      <h3>{{ index + 1 }} {{ post.title }} {{ post.id }}</h3>
+      <p>{{ post.body }}</p>
+    </div>
+    <router-view />
   </div>
 </template>
+<script>
+import { mapGetters, mapActions } from "vuex";
+import { mapState } from "vuex";
 
+export default {
+  props: {
+    currentPage: {
+      default: 1,
+    }
+  },
+  data() {
+    return {
+      perPage: 10
+    };
+  },
+  computed: {
+    ...mapGetters({
+      posts: "getAllPosts",
+      validPosts: "getValidPosts",
+      getPostByPage: "getPostByPage"
+    }),
+    pages() {
+      return this.posts.length / this.perPage;
+    },
+    showPosts() {
+      let from = 0;
+      let to = 10;
+      return this.getPostByPage(from, to);
+    },
+    test() {
+      return console.log(this.$route.params.path)
+    }
+  },
+  methods: {
+    ...mapActions({
+      loadPosts: "loadPosts"
+    })
+  },
+  mounted() {
+    this.loadPosts();
+  }
+};
+</script>
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.post {
+  display: block;
+  width: 300px;
+  margin: 10px auto;
+  border: 1px solid #000;
+  border-radius: 5px;
+  padding: 10px;
 }
 
-#nav {
-  padding: 30px;
+// Pagination
+.pagination {
+  display: inline-block;
+  margin: 10px auto;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.pagination a {
+  color: black;
+  float: left;
+  padding: 8px 16px;
+  text-decoration: none;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.pagination a.active {
+  background-color: #4caf50;
+  color: white;
+}
+
+.pagination a:hover:not(.active) {
+  background-color: #ddd;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,47 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue"
+import Vuex from "vuex"
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+Vue.use(VueAxios, axios)
 
 export default new Vuex.Store({
   state: {
-  },
-  mutations: {
+    posts: []
   },
   actions: {
+    loadPosts({ commit }) {
+      axios
+        .get('https://jsonplaceholder.typicode.com/posts')
+        .then(r => r.data)
+        .then(posts => {
+          commit('SET_POSTS', posts)
+        })
+    }
+  },
+  mutations: {
+    SET_POSTS(state, posts) {
+      state.posts = posts;
+    }
+  },
+  getters: {
+    getAllPosts(state) {
+      return state.posts;
+    },
+    getValidPosts(state) {
+      return state.posts.filter(p => {
+        return p.id >= 1 && p.id <= 10;
+      });
+    },
+    // getPostByPage: (state, getters) => (a, b) => {
+    //   return state.posts.filter(post => post.id >= a && post.id <= b)
+    // },
+    getPostByPage: (state, getters) => (a, b) => {
+      return state.posts.slice(a, b)
+    },
   },
   modules: {
+
   }
-})
+});
